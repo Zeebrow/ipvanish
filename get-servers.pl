69a2c4e284aa5a9a3f9203c9bb76217eb3c98c9e
Zeebrow <mzborowski@yahoo.com>
2021-10-17 01:12:05 -0400

    progress

--- get-servers.pl
+++ get-servers.pl
@@ -4,11 +4,14 @@ use warnings;
 use Switch;
 use Cwd;
 use Getopt::Std;
-use IpvanishServer;
+use Data::Dumper;
+use Ipvanish::Server;
+use Ipvanish::City;
 
 my @configs = ();
 my @countries = (); 
-my @cities = ();
+my %cities; 
+my @cities_found = ();
 my @servers = ();
 my $configs_dir = 'configs';
 
@@ -21,61 +24,77 @@ sub parse_configs{
     @configs = readdir $dir;
   closedir $dir;
 
+  my @baddies;
   foreach(@configs){
     # ipvanish-US-Las-Vegas-las-c38.ovpn
-    $_ =~ /(?:ipvanish-)(?<country>[A-Z]{2})-(?<city>[a-zA-Z-]*)-(?<country_sht>[a-z]{3})-(?<server_id>[a-z][0-9]{2})(?:.ovpn)$/n;
+    if($_ !~ /^.*\.ovpn$/m){
+      push(@baddies, $_);
+      next;
+    }
+
+    $_ =~ /(?:ipvanish-)(?<country>[A-Z]{2})-(?<city>[a-zA-Z-]*)-(?<city_short>[a-z]{3})-(?<server_id>[a-z][0-9]{2})(?:.ovpn)$/n;
     push(@countries, $+{country});
-    push(@cities, $+{city});
-    push(@{$country_cities{$+{country_sht}}}, $+{city});
+    push(@{$country_cities{$+{city_short}}}, $+{city});
     push(@{$city_servers{$+{city}}}, $+{server_id});
-    push(@servers, IpvanishServer->new({
+    if( grep( /$+{city_short}/, @cities_found ) ){
+      $cities{$+{city_short}}->add_server($+{server_id});
+    } else {
+      push(@cities_found, $+{city_short});
+      $cities{$+{city_short}} = City->new({
+            country => $+{country},
+            name => $+{city},
+            abbv => $+{city_short}
+          });
+      $cities{$+{city_short}}->add_server($+{server_id});
+    }
+    push(@servers, Server->new({
           filepath => $_,
           name => $+{server_id},
           country => $+{country},
           city => $+{city},
-          city_short => $+{country_sht}
+          city_short => $+{city_short}
         }));
   }
-  print "parse_configs sees ".scalar(@countries)." countries.\n";
+
+  @countries = uniq(\@countries);
 }
 
 sub print_city_servers {
-  foreach my $ct (keys(%city_servers)){
-    print "$ct: ";
-#   foreach my $srv (@{$city_servers{$ct}}){
-#     print "$srv "
-#   }
-    print "\n";
+  my $city_short = shift @_;
+  #print Dumper $cities{'atl'};
+  if( not exists( $cities{$city_short} ) ){
+    print "No such city: $city_short\n";
+    return 1;
   }
+  print "$city_short :\n";
+  foreach( sort( @{$cities{$city_short}->{server_list}} ) ){
+    print "$_ ";
+  }
+  print "\n";
+  return 0;
 }
+
 sub uniq {
   # stderr_dk kindly requests $1M should this function become an NFT.
-  
   my $ctrys = shift;
   my %seen;
 
   foreach ( @$ctrys ) {
       $seen{ $_ } = 1;
   }
-  print("keys: ".keys(%seen)."\n");
   @$ctrys = reverse( sort ( keys ( %seen)));
 }
 
 sub get_all_country_abbv{
-  print 'get_uniques sees @countries ref(\@countries) as: '.ref(\@countries)."\n"; 
-  uniq(\@countries);
-
   print "Country codes: @countries\n";
-  return;
-  foreach my $s (@servers){
-    print $s->to_string();
-  }
 }
 
 sub do_args{
   switch($ARGV[0]){
-    case "list" { print_city_servers }
+    case "list" { return print_city_servers $ARGV[1]}
     case "countries" { get_all_country_abbv } 
+    case "use" {}
+    case "down" {}
     else { print "no\n" }
   }
 }
