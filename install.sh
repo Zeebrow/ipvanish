59062d8f50718bf741934a772801dfe6625facc1
Zeebrow <mzborowski@yahoo.com>
2021-10-19 21:28:26 -0400

    troubleshoot why get_city_servers() is

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ cd "$configs_dir"
 wget https://www.ipvanish.com/software/configs/configs.zip
 unzip configs.zip
 pwd
-ls | wc -l
+ls | grep syd
+ls | grep syd | wc -l
+
 rm configs.zip
 sudo chown -R 0700 "$configs_dir"
 
