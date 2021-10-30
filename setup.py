29b99bed49437e48264dfe259065d4c6aa659424
Zeebrow <mzborowski@yahoo.com>
2021-10-30 19:10:32 -0400

    get rid of eggs; change pkg structure in prep for cli

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='ipvanish',
     version='0.0.1',
     package_dir={'':'src'},
-    #packages=find_packages(where='src')
-    packages=find_packages(include=['ipvanish'])
+    packages=find_packages(where='src')
+    #packages=find_packages(include=['ipvanish'])
 )
