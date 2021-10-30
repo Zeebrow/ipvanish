b0170a121cc45e1164d80544a2084e8807d49697
Zeebrow <mzborowski@yahoo.com>
2021-10-30 16:11:54 -0400

    saving progress before packaging headaches

--- src/processes.py
+++ src/processes.py
@@ -5,28 +5,38 @@ import tempfile
 import shutil
 import zipfile
 import io
-# TODO: refactor to save requests dependency 
+import logging
+from pathlib import Path
 from requests import get
+
 from ipvanish import get_ovpn_config_dir
+
+logger = logging.getLogger(__name__)
+
 
 def update_configs(cfg_dir=get_ovpn_config_dir()):
     # backup existing config
-    cfg_dir_bkup = tempfile.TemporaryDirectory(prefix='ipvanish-', suffix='-conf.bkup' )
-    shutil.copytree(src=cfg_dir, dst=cfg_dir_bkup+'/configs', copy_function=shutil.copy2)
-    
+#    cfg_dir_bkup = tempfile.TemporaryDirectory(prefix='ipvanish-', suffix='-conf.bkup' )
+#    cfg_dir_bkup = Path(cfg_dir_bkup.name )
+    cfg_dir_bkup = Path(tempfile.mkdtemp(prefix='ipvanish-cfg-bkup'))
+    shutil.copytree(src=cfg_dir, dst=cfg_dir_bkup / 'configs', copy_function=shutil.copy2)
+    print(cfg_dir_bkup)
     try:
         # TODO: host proxy for configs.zip
         url = "https://www.ipvanish.com/software/configs/configs.zip"
-        r = requests.get(url)
+        r = get(url)
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall(cfg_dir)
+        raise FileNotFoundError("test exception")
     except Exception as e:
         print(e)
+        logger.critical(e)
         # compress backup
         #TODO: nowzers -> datetime
         nowzers='ryenow'
-        shutil.make_archive(f'configs-bkup-{nowzers}.zip')
-        print(f"backup config accessible at '{cfg_dir_bkup.name}'")
+        archive_filename = f'configs-bkup-{nowzers}'
+        shutil.make_archive(archive_filename, format='zip', root_dir=cfg_dir_bkup)
+        print(f"backup config accessible at '{cfg_dir_bkup / archive_filename}'")
         return False
     
 def start(cfgfile, upfile=None):
@@ -50,11 +60,13 @@ def start(cfgfile, upfile=None):
     cmd += ["--config", cfgfile]
     cmd += ["--user", getpass.getuser()]
     cmd += autharg
-    print(cmd)
-    with open("ipvanish.log", "a+") as logf:
+    logger.debug(f"running command: {cmd}")
+    pidfile = "ipvanish.pid"
+    logfile = "ipvanish.log"
+    with open(logfile, "a+") as logf:
         c = subprocess.Popen(cmd, stdout=logf, stderr=logf)
-    with open("ipvanish.pid", "w+") as pidf:
-        print(c.pid)
+    with open(pidfile, "w+") as pidf:
+        logger.info(f"ipvanish pid '{c.pid}' saved to file '{pidfile}'")
         pidf.write(str(c.pid))
 
 
@@ -64,18 +76,19 @@ def stop(self):
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 2:
-        if "start" == sys.argv[1]:
-            start(cfgfile="tests/configs/ipvanish-RS-Belgrade-beg-c01.ovpn", upfile="/home/zeebrow/.up")
-        elif "stop" == sys.argv[1]:
-            stop()
-        else:
-            print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print("no")
-        sys.exit(1)
+    update_configs()
+#    import sys
+#    if len(sys.argv) == 2:
+#        if "start" == sys.argv[1]:
+#            start(cfgfile="tests/configs/ipvanish-RS-Belgrade-beg-c01.ovpn", upfile="/home/zeebrow/.up")
+#        elif "stop" == sys.argv[1]:
+#            stop()
+#        else:
+#            print("Unknown command")
+#            sys.exit(2)
+#        sys.exit(0)
+#    else:
+#        print("no")
+#        sys.exit(1)
 
 # sudo openvpn --config configs/ipvanish-RS-Belgrade-beg-c01.ovpn --ca configs/ca.ipvanish.com.crt     --user zeebrow --auth-user-pass "$HOME/.up"
