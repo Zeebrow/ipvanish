e4b8b6fe359d62f9584be8038b7cf9ceb04fd44b
Zeebrow <mzborowski@yahoo.com>
2021-10-29 23:37:39 -0400

    remove the bunch of files from tests

--- tests/fixtures/fixt_cfg_dir.py
+++ tests/fixtures/fixt_cfg_dir.py
@@ -1,5 +1,58 @@
 import pytest
 import random
+import string
+from collections import defaultdict
+
+def create_config_filename(namelen=10, **kw) -> string:
+    """
+    kwargs:  country city, abv
+    """ 
+    for k in kw.keys():
+        if k not in ['country', 'city', 'abv', 'server']:
+            print(f"WARN: ignoring keyword argument '{k}'")
+            continue
+    c,a = city_abv_pair(city_name_len=namelen)
+    return f"ipvanish-{kw.get('country', 'US')}-{kw.get('city',c)}-{kw.get('abv',a)}-{kw.get('server', random_server_name())}.ovpn"
+
+def ascii(num=1, case=None) -> str:
+    """
+    Generate random ascii characters of definite length, or in a pattern
+    """
+    if case in ('u', 'upper'):
+        _f = lambda: random.choice(string.ascii_uppercase)
+    elif case in ('l', 'lower'):
+        _f = lambda: random.choice(string.ascii_lowercase)
+    elif case in ('p', 'proper'):
+        _f = lambda: random.choice(string.ascii_lowercase)
+        return random.choice(string.ascii_uppercase) + ''.join([_f() for i in range(num-1)])
+    else:
+        _f = lambda: random.choice(string.ascii_letters)
+    return ''.join([_f() for i in range(num)])
+
+def srvr_digits(num=2) -> str:
+    if num < 1:
+        raise ValueError("number of digits must be greater than 0")
+    return "%02d" % random.randint(0, 9*(10**(num-1)))
+
+def city_abv_pair(city_name_len=10) -> tuple:
+    name = ascii(city_name_len, 'proper')
+    abv = name[0:2].lower() + name[-1]
+    return name,abv
+
+def random_server_name():
+    return ascii(1,'l') + srvr_digits(2)
+
+def create_fake_config_filename(namelen=10) -> str:
+    city = city_abv_pair(city_name_len=namelen)
+    filename = f"ipvanish-{ascii(2, 'u')}-{city[0]}-{city[1]}-{ascii(1,'l') + srvr_digits(2)}.ovpn"
+    return filename
+
+@pytest.fixture
+def fixture_fake_config_filenames(count=1, namelen=10, **kwargs):
+    rtn = []
+    for i in count:
+        rtn.append(create_config_filename(namelen=namelen, **kwargs))
+    return rtn
 
 @pytest.fixture
 def fixture_config_dirs(tmp_path, range_lo=5, range_hi=10):
@@ -13,11 +66,38 @@ def fixture_config_dirs(tmp_path, range_lo=5, range_hi=10):
     num_files = random.randint(range_lo,range_hi)
     for f in range(num_files):
         f1 = tdir / f"file{f}.ovpn"
+        f1 = tdir / create_config_filename()
         f1.touch()
     # make some other files too
     for i in range(random.randint(1,3)):
         randofile = tdir/f"rando{i}"
         randofile.touch()
-    
+
     return (tdir, num_files)
+
+if __name__ == '__main__':
+    # sanity checks
+    print()
+    print(f"{ascii(2,'l')=}")
+    print(f"{ascii(2,'u')=}")
+    print(f"{ascii(10,'p')=}")
+    print()
+    print(f"{create_config_filename(namelen=15, country='US')=}")
+    print(f"{create_config_filename(country='US')=}")
+    print(f"{create_config_filename(city='Abbudabi')=}")
+    print(f"{create_config_filename(abv='adb')=}")
+    print(f"{create_config_filename(server='a01')=}")
+    print()
+    print(f"{srvr_digits(1)=}")
+    print(f"{srvr_digits(2)=}")
+    print()
+    print(f"{city_abv_pair()=}")
+    print(f"{city_abv_pair(15)=}")
+    print()
+    print(f"{random_server_name()=}")
+    print()
+    print(f"{create_config_filename()=}")
+    print(f"{create_config_filename(5)=}")
+    print(f"{create_config_filename(15)=}")
+    print(f"{create_config_filename(20)=}")
 
--- tests/test_configurable_global_vars.py
+++ tests/test_configurable_global_vars.py
@@ -7,10 +7,9 @@ from ipvanish import get_ovpn_config_dir, list_configs
 import random
 from fixtures.fixt_cfg_dir import fixture_config_dirs
 
-cwd = Path(os.path.dirname(__file__))
-config_testdata = str(cwd / "empty_confgfiles")
+#cwd = Path(os.path.dirname(__file__))
+#config_testdata = str(cwd / "empty_confgfiles")
 #default_config_dir = str(Path( os.path.expanduser('~')) / ".config/ipvanish/configs")
-default_xdg_config_home =  str(Path( os.path.expanduser('~')) / ".config")
 
 # TODO: change name of func from list_configs() to list_configs()
 def test_list_configs_is_configurable(fixture_config_dirs):
@@ -29,23 +28,19 @@ def test_default_config_dir_is_user_dotconfig(fixture_config_dirs):
         m.delenv("XDG_CONFIG_HOME", raising=False)
         assert get_ovpn_config_dir(str(cfg_dir)) == str(cfg_dir)
 
-def test_IPVANISH_CONFIG_DIR_overrides_XDG():
+def test_IPVANISH_CONFIG_DIR_overrides_XDG(fixture_config_dirs):
+    default_xdg_config_home =  str(Path( os.path.expanduser('~')) / ".config")
+    cfg_dir, _ = fixture_config_dirs
     monkeypatch = MonkeyPatch()
     with monkeypatch.context() as m:
-        m.setenv("IPVANISH_CONFIG_DIR", config_testdata)
+        m.setenv("IPVANISH_CONFIG_DIR", str(cfg_dir))
         m.setenv("XDG_CONFIG_HOME", default_xdg_config_home)
         assert get_ovpn_config_dir() != str(Path( default_xdg_config_home ) / "ipvanish/configs")
-        assert get_ovpn_config_dir() == config_testdata
+        assert get_ovpn_config_dir() == str(cfg_dir)
 
 def test_cli_arg_overrides_IPVANISH_CONFIG_DIR():
     pass
 
-def test_list_configs():
-    monkeypatch = MonkeyPatch()
-    with monkeypatch.context() as m:
-        m.setenv("IPVANISH_CONFIG_DIR", config_testdata)
-        cfg_list = list_configs()
-    assert len(cfg_list) == 1962
         
 if __name__ == '__main__':
     pytest.main(['--trace',__file__])
