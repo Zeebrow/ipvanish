e016c54e7a036e0f1487ae211f7db80d2a3b86ef
Zeebrow <mzborowski@yahoo.com>
2021-10-30 21:25:26 -0400

    saving before adding click echo

--- src/scripts/ipvanish.py
+++ src/scripts/ipvanish.py
@@ -1,9 +1,32 @@
 import click
 
+from ipvanish import get_city_servers, get_countries, get_country_cities
+
+from .cli_helpers import column_print
 
 @click.group()
 def main():
     return
+
+
+@click.command()
+def get_ctry():
+    c = get_countries()
+    testlist = ['asdf']
+    c = list(c) + testlist
+    print(c)
+    d = []
+    hm = []
+    for i in c:
+        d.append('green')
+    for j in zip(c,d):
+        hm.append(j)
+    print(hm)
+    column_print(hm)
+
+@click.command()
+def get_servers():
+    get_city_servers()
 
 @click.command()
 def yo():
@@ -11,6 +34,8 @@ def yo():
     
 
 main.add_command(yo, name='hi')
+main.add_command(get_servers, name='srv')
+main.add_command(get_ctry, name='ctry')
 
 if __name__ == '__main__':
     main()
